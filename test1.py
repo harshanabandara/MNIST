8e6bde0f3dd783005c1dceb6067d3f06dd6073c3
HarshanaBandara <e16049@pdn.ac.lk>
2021-06-07 01:17:33 +0530

    Download dataset to MNIST problem in machine learning

--- test1.py
+++ test1.py
@@ -1,7 +1,13 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from numpy.core.numeric import identity
+import numpy as np 
+import matplotlib.pyplot as plt 
+import torch
+from tqdm.notebook import tqdm
+from torchvision import datasets,transforms
 
-rand = np.random.rand(50,50)
-plt.imshow(rand)
-plt.show()
+mnist_train = datasets.MNIST(root = "./datasets" , train = True , transform = transforms.ToTensor ,download = True)
+mnist_test = datasets.MNIST(root = "./datasets" , train = False , transform = transforms.ToTensor ,download = True)
+
+print(f'number of MNIST training examples : {format(len(mnist_train))}')
+print(f'number of MNIST test examples : {format(len(mnist_test))}')
+
+image , label = mnist_train[3] 
